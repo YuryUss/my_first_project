e3d211069eb691f12080050dd9245af342cab6bd
МаJoR <uss.yura@mail.ru>
2020-12-17 20:54:57 +0300

    иой первый код

--- 1.py
+++ 1.py
@@ -4,3 +4,11 @@ def E2(a):
    return a%2==0
 print(list(
 filter(E2, [1, 500, 6, 4, 7])))
+
+
+frrom functools import reduce
+def E3(a, b):
+    c = a + b
+    return c
+print(
+reduce(E3,[23, 2, 3],1))
